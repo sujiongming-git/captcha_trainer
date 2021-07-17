fa14ebd42aad91cd97724c7bc275d79b1008facc
jm.su <jm.su@aftership.com>
2021-07-17 22:11:30 +0800

    feat: add taiwanpost

---
+++ scripts/converter.py
@@ -0,0 +1,31 @@
+import re
+import os
+import hashlib
+
+# 训练集路径
+root = "/Users/jm.su/Documents/code/captcha_trainer/scripts/taiwan-post/"
+all_files = os.listdir(root)
+
+for file in all_files:
+    old_path = os.path.join(root, file)
+    print(old_path)
+
+    if "-" in file:
+        file = file.split("-", 1)[-1]
+
+    print(file)
+    # continue
+
+    # 已被修改过忽略
+    if len(file.split(".")[0]) > 32:
+        continue
+
+    # 采用标注_文件md5码.图片后缀 进行命名
+    with open(old_path, "rb") as f:
+        _id = hashlib.md5(f.read()).hexdigest()
+    new_path = os.path.join(root, file.replace(".", "_{}.".format(_id)))
+
+    # 重复标签的时候会出现形如：abcd (1).jpg 这种形式的文件名
+    new_path = re.sub(" \(\d+\)", "", new_path)
+    print(new_path)
+    os.rename(old_path, new_path)
